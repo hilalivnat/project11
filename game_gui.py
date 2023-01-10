b889fe56bfb970064085d06388a019408b422318
hilalivnat <hila.livnat@mail.huji.ac.il>
2023-01-10 19:07:59 +0200

    style with grid

--- game_gui.py
+++ game_gui.py
@@ -1,38 +1,62 @@
 import tkinter
+import uuid
+from typing import *
+from styles import *
+
 class GameGui:
     def __init__(self) -> None:
+        # helper variables
+        # self._current_word = ""
+        self._found_words = []
+        # tkinter vars
         root = tkinter.Tk()
         root.title("Boogle")
-        # root.resizable(False, False)
+        root.geometry("600x600")
         self.__root = root
-        
-        self._title_label = tkinter.Label(self.__root, text="Boogle", font=("Courier", 30))
-        self._title_label.grid(row=0, column=0)
-        
-        self._buttons_frame = tkinter.Frame(self.__root)
-        # tkinter.Grid.grid_configure(self._button_frame,column=4, row=4)
-        self._buttons_frame.grid(row=1, column=0)
-        # self._buttons_frame.grid_configure()
 
-        self._display_word = tkinter.Label(self.__root, text="kk")
-        self._display_word.grid(row=2, column=0)
+        self._main_frame = tkinter.Frame(self.__root, width=100, height=100)
+        self._main_frame.grid(column=0, row=0, sticky=tkinter.NSEW)
+        
+        self._title_label = tkinter.Label(self._main_frame, text="Boogle" ,**DISPLAY_WORD)
+        self._title_label.grid(row=0, column=0, sticky=tkinter.NSEW)
+        
+        # self._display_word = tkinter.Label(self._main_frame, text="",**DISPLAY_WORD)
+        # self._display_word.grid(row=1, column=0)
+        self._score_display = tkinter.Label(self._main_frame ,**DISPLAY_WORD)
+        self._score_display.grid(row=1, column=0, sticky=tkinter.NSEW)
+
+        self._buttons_frame = tkinter.Frame(self._main_frame)
+        self._buttons_frame.grid(row=2, column=2, sticky=tkinter.NSEW)
+        
+        self._words_display = tkinter.Label(self._main_frame ,**FOUND_WORDS)
+        self._words_display.grid(row=3, column=2, sticky=tkinter.NSEW)
+
+        self._buttons:Dict[tkinter.Button] = dict()
 
 
 
     def run(self):
         self.__root.mainloop()
 
-    def create_button_command(self,f, cell):
+    def create_button_command(self,f, cell, btn_id):
         def handler():
-            f(cell) 
+            # self._current_word += char
+            # self._display_word["text"] = self._current_word
+            self._buttons[btn_id]["state"] = "disabled"
+            self._buttons[btn_id]["bg"] = DISABLED_COLOR
+            word, score = f(cell) 
+            if word:
+                self._found_words.append(word)
+                self.update_board(score)
         return handler       
 
     def _create_button(self, coord, char, f):
+        btn_id = uuid.uuid4()
         button = tkinter.Button(self._buttons_frame, 
-                                text=char, font=("Asimon", 15), 
-                                command= self.create_button_command(f, coord))
-        button.grid(row=coord[0] + 2, column=coord[1],sticky=tkinter.NSEW, rowspan=1, columnspan=1)
-        # button.pack()
+                                text=char, 
+                                command= self.create_button_command(f, coord, btn_id), **BUTTON_STYLE)
+        button.grid(row=coord[0], column=coord[1],sticky=tkinter.NSEW, rowspan=1, columnspan=1)
+        self._buttons[btn_id] = button
 
 
     def create_board(self,board, f):
@@ -41,6 +65,17 @@ class GameGui:
         for row_i, row in enumerate(board):
             for col_i, char in enumerate(row):
                 self._create_button((row_i, col_i), char, f)
+
+
+    def update_board(self, score):
+        for btn in self._buttons.values():
+            btn["state"] = "normal"
+            btn["bg"] = BTN_BG
+
+        self._words_display["text"] ="found words:\n" + "\n".join(self._found_words)   
+        self._score_display["text"] = f'Score: {score}'
+
+
 
         
             
---
+++ styles.py
@@ -0,0 +1,23 @@
+TEXT_COLOR = "#052266"
+DISABLED_COLOR = "#45a9ba"
+BTN_BG = "#d5f9ff"
+
+DISPLAY_WORD ={
+    "font":("Asimon", 20),
+    "fg":TEXT_COLOR
+}
+
+BUTTON_STYLE ={
+    "font":("Asimon", 15),
+    "bg":BTN_BG, 
+    "fg": TEXT_COLOR, 
+    "width":4, 
+    "height":2
+}
+
+FOUND_WORDS = {
+    "font":("Asimon", 15),
+    "fg":TEXT_COLOR, 
+}
+
+
