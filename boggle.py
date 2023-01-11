c0218d37ec4acea82f5afeb8c58d548e6c0d12d0
hilalivnat <hila.livnat@mail.huji.ac.il>
2023-01-11 12:30:24 +0200

    added clear btn

--- boggle.py
+++ boggle.py
@@ -20,7 +20,7 @@ class BoggleGame:
     
     def start_new_game(self):
         board = self._game_model.get_board()
-        self._game_gui.create_board(board, self._game_model.do_letter_clicked)   
+        self._game_gui.create_board(board, self._game_model.do_letter_clicked, self._game_model.clear_choice)   
         self._game_gui.run()
 
 
--- game_gui.py
+++ game_gui.py
@@ -19,7 +19,7 @@ from styles import *
 class GameGui:
     def __init__(self) -> None:
         # helper variables
-        # self._current_word = ""
+        self._current_word = ""
         self._found_words = []
         # tkinter vars
         root = tkinter.Tk()
@@ -33,13 +33,17 @@ class GameGui:
         self._title_label = tkinter.Label(self._main_frame, text="Boogle", **DISPLAY_WORD)
         self._title_label.pack(fill=tkinter.BOTH, expand=True)
 
-        # self._display_word = tkinter.Label(self._main_frame, text="",**DISPLAY_WORD)
-        # self._display_word.grid(row=1, column=0)
+        self._display_word = tkinter.Label(self._main_frame, text="",**DISPLAY_WORD)
+        self._display_word.pack(fill=tkinter.BOTH, expand=True)
+       
         self._score_display = tkinter.Label(self._main_frame, **DISPLAY_WORD)
         self._score_display.pack(fill=tkinter.BOTH, expand=True)
 
         self._buttons_frame = tkinter.Frame(self._main_frame)
         self._buttons_frame.pack(fill=tkinter.BOTH, expand=True)
+
+        self._clear_btn = tkinter.Button(self._main_frame, text="Clear Word")
+        self._clear_btn.pack(fill=tkinter.BOTH, expand=True)
 
         self._words_display = tkinter.Label(self._main_frame, **FOUND_WORDS)
         self._words_display.pack(fill=tkinter.BOTH, expand=True)
@@ -49,16 +53,18 @@ class GameGui:
     def run(self):
         self.__root.mainloop()
 
-    def create_button_command(self, f, cell, btn_id):
+    def create_button_command(self, f, cell, char, btn_id):
         def handler():
-            # self._current_word += char
-            # self._display_word["text"] = self._current_word
+            self._current_word += char
             self._buttons[btn_id]["state"] = "disabled"
             self._buttons[btn_id]["bg"] = DISABLED_COLOR
             word, score = f(cell)
             if word:
                 self._found_words.append(word)
+                self._current_word = ""
                 self.update_board(score)
+
+            self._display_word["text"] = self._current_word
 
         return handler
 
@@ -66,24 +72,35 @@ class GameGui:
         btn_id = uuid.uuid4()
         button = tkinter.Button(self._buttons_frame,
                                 text=char,
-                                command=self.create_button_command(f, coord, btn_id), **BUTTON_STYLE)
+                                command=self.create_button_command(f, coord, char, btn_id), **BUTTON_STYLE)
         button.grid(row=coord[0], column=coord[1], sticky=tkinter.NSEW, rowspan=1, columnspan=1)
         self._buttons[btn_id] = button
 
-    def create_board(self, board, f):
+    def create_board(self, board, click_on_btn_f, clear_f):
         # for i in range(4):
         # tkinter.Grid.grid_configure(self._buttons_frame,column=4, row=4)
         for row_i, row in enumerate(board):
             for col_i, char in enumerate(row):
                 self._buttons_frame.grid_columnconfigure(col_i, weight=1, uniform="True")
                 self._buttons_frame.grid_rowconfigure(row_i, weight=1, uniform="True")
-                self._create_button((row_i, col_i), char, f)
+                self._create_button((row_i, col_i), char, click_on_btn_f)
+        
+        def btn_clear_func():
+            clear_f()
+            self._current_word =""
+            self._display_word["text"] = self._current_word
+            self._return_buttons_to_normal_state()
+            
+        self._clear_btn["command"] = btn_clear_func
 
-    def update_board(self, score):
+            
+    def _return_buttons_to_normal_state(self):
         for btn in self._buttons.values():
             btn["state"] = "normal"
             btn["bg"] = BTN_BG
 
+    def update_board(self, score):
+        self._return_buttons_to_normal_state()
         self._words_display["text"] = "found words:\n" + "\n".join(self._found_words)
         self._score_display["text"] = f'Score: {score}'
 
